56533fae20c1af6626b1b38105350cde4803d45b
Sam Neth <sam.neth@rumbleentertainment.com>
2019-03-21 11:22:34 -0700

    make this usable for testing

--- grails-app/controllers/player/service/PlayerController.groovy
+++ grails-app/controllers/player/service/PlayerController.groovy
@@ -1,10 +1,23 @@
 package player.service
 
+import grails.converters.JSON
 import org.springframework.util.MimeTypeUtils
 
 class PlayerController {
 
     def index() {
+        def responseData = [
+            success: true,
+            requestId: '03B31646-611E-4BC3-AF92-8B2982443799',
+            remoteAddr: request.remoteAddr,
+            geoipAddr: request.remoteAddr,
+            country: 'US',
+            accountId: JSON.parse(params.manifest).identity.installId,
+            dateCreated: '\'' + System.currentTimeMillis() + '\'',
+            accessToken: UUID.randomUUID().toString(),
+            assetPath: 'https://rumble-game-alliance-dist.s3.amazonaws.com/client/',
+            clientvars: [:]
+        ]
         def boundary = MimeTypeUtils.generateMultipartBoundaryString()
         response.setContentType('multipart/related; boundary="' + boundary + '"')
         def out = response.writer
@@ -16,11 +29,10 @@ class PlayerController {
         out.write('Content-Disposition: inline')
         out.write('\r\n')
         out.write('\r\n')
-        out.write(sampleResponseBody)
+        out.write((responseData as JSON).toString())
         request.parameterNames.each { name->
             sendFile(out, boundary, name, request.getParameter(name))
         }
-        sendFile(out, boundary, 'sample', sampleResponseBody)
         out.write('\r\n')
         out.write('--')
         out.write(boundary)
@@ -39,25 +51,9 @@ class PlayerController {
         out.write(name)
         out.write('"; filename="')
         out.write(name)
-        out.write('.json"')
+        out.write('.dat"')
         out.write('\r\n')
         out.write('\r\n')
         out.write(content)
     }
-
-    def sampleResponseBody = '{\n' +
-            '\t"success": true,\n' +
-            '\t"requestId": "03B31646-611E-4BC3-AF92-8B2982443799",\n' +
-            '\t"remoteAddr": "192.168.1.27",\n' +
-            '\t"geoipAddr": "55.66.77.88",\n' +
-            '\t"country": "US",\n' +
-            '\t"accountId": "CB600CC6-FC0C-44EF-A51B-776585E8CBB7",\n' +
-            '\t"dateCreated": "13826116102651",\n' +
-            '\t"accessToken": "FBDECEFC-AA43-4703-8DDD-4E6EC181BB8F",\n' +
-            '\t"assetPath": "https://rumble-game-alliance-dist.s3.amazonaws.com/client/",\n' +
-            '\t"clientvars":\n' +
-            '\t{\n' +
-            '\t\t...\n' +
-            '\t}\n' +
-            '}'
 }
