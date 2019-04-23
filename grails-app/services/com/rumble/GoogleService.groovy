da0b313265b1e1b7f7d6a710766e483bc3e60435
Nhathy Ngo <nhathy.ngo@rumbleentertainment.com>
2019-04-23 11:26:24 -0700

    google user validation

--- grails-app/services/com/rumble/GoogleService.groovy
+++ grails-app/services/com/rumble/GoogleService.groovy
@@ -1,11 +1,34 @@
 package com.rumble
 
-//import grails.gorm.transactions.Transactional
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.Method
 
-//@Transactional
 class GoogleService {
 
+    static private VALIDATE_TOKEN_URL = System.getProperty("GOOGLE_VALIDATE_TOKEN_URL") ?: System.getenv("GOOGLE_VALIDATE_TOKEN_URL")
+    static private APP_ID = System.getProperty("GOOGLE_APP_ID") ?: System.getenv("GOOGLE_APP_ID")
     def validateAccount(token) {
-        return true
+        def http = new HTTPBuilder(VALIDATE_TOKEN_URL + APP_ID + "/verify/")
+        http.client.params.setParameter("http.connection.timeout", 5000)
+        http.client.params.setParameter("http.socket.timeout", 5000)
+        http.request(Method.GET, ContentType.JSON) {
+            headers.Authorization = "OAuth ${token}"
+
+            // example response:
+            // {{
+            //  "kind": "games#applicationVerifyResponse",
+            //  "player_id": "g11282747315603104675"
+            // }}
+            response.success = { resp, reader ->
+                return reader.player_id
+            }
+
+            response.failure = { resp, reader ->
+                // TODO: Log error
+                return false
+            }
+        }
+    }
     }
 }
