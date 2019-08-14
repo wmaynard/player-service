5f7b1b6de9e69775f67ad850b519bd3810c5435d
Nhathy Ngo <nhathy.ngo@rumbleentertainment.com>
2019-08-14 10:23:49 -0700

    Add inbox and tracking components

--- grails-app/services/com/rumble/api/services/AccountService.groovy
+++ grails-app/services/com/rumble/api/services/AccountService.groovy
@@ -78,8 +78,10 @@ class AccountService {
                 "chests",
                 "challenges",
                 "heroes",
+                "inbox",
                 "store",
                 "summary",
+                "tracking",
                 "tutorials",
                 "wallet"
         ]
