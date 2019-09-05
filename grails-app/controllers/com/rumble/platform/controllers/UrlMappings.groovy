5011d53aee38fc025cee68040135d31cc01fd33b
Sam Neth <sam.neth@rumbleentertainment.com>
2019-09-05 13:04:05 -0700

    move exception classes and error controller into new platform-common

--- grails-app/controllers/com/rumble/platform/controllers/UrlMappings.groovy
+++ grails-app/controllers/com/rumble/platform/controllers/UrlMappings.groovy
@@ -5,16 +5,14 @@ class UrlMappings {
     static mappings = {
         "/admin/player/$action"(controller: 'adminPlayer')
 
-
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(controller: 'platformError', action: 'notFound')
-
-        "404"(controller: 'platformError', action: 'notFound')
-        "500"(controller: 'platformError', action: 'uncaughtException')
+        "/"(controller: 'error', action: 'notFound')
+        "404"(controller: 'error', action: 'notFound')
+        "500"(controller: 'error', action: 'uncaughtException')
     }
 }
