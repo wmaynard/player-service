b42b7e01a8f7d614a0733f492d9855ff23e52ff5
Sam Neth <sam.neth@rumbleentertainment.com>
2019-09-14 12:44:09 -0700

    add server auth for admin controller

--- grails-app/controllers/com/rumble/api/controllers/AdminPlayerController.groovy
+++ grails-app/controllers/com/rumble/api/controllers/AdminPlayerController.groovy
@@ -3,11 +3,15 @@ package com.rumble.api.controllers
 import grails.converters.JSON
 
 class AdminPlayerController {
+    def authService
     def accountService
     def paramsService
     def profileService
 
     def search() {
+
+        authService.checkServerAuth(request)
+
         paramsService.require(params, 's')
 
         def results = accountService.find(params.s)
@@ -21,6 +25,9 @@ class AdminPlayerController {
     }
 
     def details(){
+
+        authService.checkServerAuth(request)
+
         paramsService.require(params, 'id')
 
         def account = accountService.find(params.id)?.first()
