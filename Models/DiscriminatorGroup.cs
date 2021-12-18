4b98e1ccd492f5254f1bcd22cb9389c4c40c646d
Will Maynard <will@willmaynard.com>
2021-12-17 17:58:24 -0800

    Refactor DiscriminatorService

---
+++ Models/DiscriminatorGroup.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using Rumble.Platform.Common.Web;
+
+namespace PlayerService.Models
+{
+	public class DiscriminatorGroup : PlatformCollectionDocument
+	{
+		public const string DB_KEY_NUMBER = "number";
+		public const string DB_KEY_MEMBERS = "members";
+
+		// TODO: The naming of this collection predates current Platform practices; update these to be consistent with nomenclature rules
+		public const string FRIENDLY_KEY_NUMBER = DB_KEY_NUMBER;
+		public const string FRIENDLY_KEY_MEMBERS = DB_KEY_MEMBERS;
+
+		[BsonElement(DB_KEY_MEMBERS)]
+		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_MEMBERS), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public List<DiscriminatorMember> Members { get; private set; }
+		
+		[BsonElement(DB_KEY_NUMBER)]
+		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_NUMBER)]
+		public int Number { get; private set; }
+
+		public DiscriminatorGroup(int number)
+		{
+			Number = number;
+			Members = new List<DiscriminatorMember>();
+		}
+		
+		public DiscriminatorGroup(int number, DiscriminatorMember member) : this(number) => Members.Add(member);
+
+		public bool HasScreenname(string screenname) => Members.Any(member => member.ScreenName == screenname);
+		// public bool HasMember(string accountId) => Members.Any(member => member.AccountId == accountId);
+		public void RemoveMember(string accountId) => Members.RemoveAll(member => member.AccountId == accountId);
+		public void AddMember(string accountId, string screenname) => Members.Add(new DiscriminatorMember(accountId, screenname));
+		public void UpdateMember(Player player) => Members.First(m => m.AccountId == player.AccountId).Update(player.Screenname);
+	}
+}
--- Services/DiscriminatorService.cs
+++ Services/DiscriminatorService.cs
@@ -31,22 +31,27 @@ namespace PlayerService.Services
 
 		private DiscriminatorGroup Find(Player player) => FindOne(group => group.Members.Any(member => member.AccountId == player.AccountId));
 
-		public int Lookup(Player player) => Find(player)?.Number ?? TryAssign(player);
-
-		private int TryAssign(Player player)
+		private int Assign(Player player)
 		{
 			List<int> attempted = new List<int>();
 			for (int i = 0; i < MAX_ASSIGNMENT_ATTEMPTS; i++)
-				if (Assign(player.AccountId, player.Screenname, out int output))
+				if (TryAssign(player.AccountId, player.Screenname, out int output))
 					return output;
 				else
 					attempted.Add(output);
 			throw new DiscriminatorUnavailableException(player.AccountId, attempted);
 		}
+		public int Lookup(Player player) => Find(player)?.Number ?? Assign(player);
 
-		private bool Assign(string accountId, string screenname, out int discriminator)
+		/// <summary>
+		/// Attempts to assign a discriminator to a player.  It the player's screenname is already taken by
+		/// another accountId, returns false.
+		/// </summary>
+		/// <returns>True if successful.</returns>
+		private bool TryAssign(string accountId, string screenname, out int discriminator)
 		{
-			int target = discriminator = new Random().Next(MINIMUM_VALUE, MAXIMUM_VALUE); // We need a local var too; out parameters can't be used in lambda expressions.
+			// We need to declare local var 'target' too; out parameters can't be used in lambda expressions.
+			int target = discriminator = new Random().Next(MINIMUM_VALUE, MAXIMUM_VALUE);
 
 			DiscriminatorGroup[] previous = Find(group => group.Members.Any(member => member.AccountId == accountId));
 			DiscriminatorGroup next = FindOne(group => group.Number == target);
@@ -69,6 +74,9 @@ namespace PlayerService.Services
 			return true;
 		}
 
+		/// <summary>
+		/// Updates a player's screenname, assigning a new discriminator if necessary.
+		/// </summary>
 		public int Update(Player player)
 		{
 			DiscriminatorGroup existing = Find(player);
@@ -76,8 +84,6 @@ namespace PlayerService.Services
 			if (!existing.HasScreenname(player.Screenname))
 			{
 				existing.UpdateMember(player);
-				existing.RemoveMember(player.AccountId);					// removes
-				existing.AddMember(player.AccountId, player.Screenname);
 				Update(existing);
 			}
 			else if (existing.Members.First(m => m.ScreenName == player.Screenname).AccountId == player.AccountId)
@@ -86,69 +92,9 @@ namespace PlayerService.Services
 			{
 				existing.RemoveMember(player.AccountId);
 				Update(existing);
-				return TryAssign(player);
+				return Assign(player);
 			}
 			return existing.Number;
 		}
-	}
-
-	public class DiscriminatorGroup : PlatformCollectionDocument
-	{
-		public const string DB_KEY_NUMBER = "number";
-		public const string DB_KEY_MEMBERS = "members";
-
-		// TODO: The naming of this collection predates current Platform practices; update these to be consistent with nomenclature rules
-		public const string FRIENDLY_KEY_NUMBER = DB_KEY_NUMBER;
-		public const string FRIENDLY_KEY_MEMBERS = DB_KEY_MEMBERS;
-
-		[BsonElement(DB_KEY_MEMBERS)]
-		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_MEMBERS), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-		public List<DiscriminatorMember> Members { get; private set; }
-		
-		[BsonElement(DB_KEY_NUMBER)]
-		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_NUMBER)]
-		public int Number { get; private set; }
-
-		public DiscriminatorGroup(int number)
-		{
-			Number = number;
-			Members = new List<DiscriminatorMember>();
-		}
-
-		public DiscriminatorGroup(int number, DiscriminatorMember member) : this(number)
-		{
-			Members.Add(member);
-		}
-
-		public bool HasScreenname(string screenname) => Members.Any(member => member.ScreenName == screenname);
-		// public bool HasMember(string accountId) => Members.Any(member => member.AccountId == accountId);
-		public void RemoveMember(string accountId) => Members.RemoveAll(member => member.AccountId == accountId);
-		public void AddMember(string accountId, string screenname) => Members.Add(new DiscriminatorMember(accountId, screenname));
-		public void UpdateMember(Player player) => Members.First(m => m.AccountId == player.AccountId).Update(player.Screenname);
-	}
-
-	public class DiscriminatorMember : PlatformDataModel
-	{
-		public const string DB_KEY_ACCOUNT_ID = "aid";
-		public const string DB_KEY_SCREENNAME = "sn";
-
-		public const string FRIENDLY_KEY_ACCOUNT_ID = "accountId";
-		public const string FRIENDLY_KEY_SCREENNAME = "screenname";
-		
-		[BsonElement(DB_KEY_ACCOUNT_ID)]
-		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_ACCOUNT_ID)]
-		public string AccountId { get; private set; }
-		
-		[BsonElement(DB_KEY_SCREENNAME)]
-		[JsonInclude, JsonPropertyName(FRIENDLY_KEY_SCREENNAME)]
-		public string ScreenName { get; private set; }
-
-		public DiscriminatorMember(string accountId, string screenname)
-		{
-			AccountId = accountId;
-			ScreenName = screenname;
-		}
-
-		public void Update(string screenname) => ScreenName = screenname;
 	}
 }
