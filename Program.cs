b85d2a85ab1f20a33b344ee7f1c6edceaefd1509
Will Maynard <will@willmaynard.com>
2022-03-04 11:41:53 -0800

    Add -version to cli args

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
+using System;
+using System.Linq;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace PlayerService;
 public class Program
 {
-	public static void Main(string[] args)
+public static void Main(string[] args)
+{
+	if (args.Contains("-version"))
 	{
-		CreateHostBuilder(args).Build().Run();
+		AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+		Console.WriteLine($"{assembly.Name}:{assembly.Version}");
+		return;
 	}
+	CreateHostBuilder(args).Build().Run();
+}
 
 	public static IHostBuilder CreateHostBuilder(string[] args) =>
 		Host.CreateDefaultBuilder(args)
